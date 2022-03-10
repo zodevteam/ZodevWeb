f2e215f7cf14a473626511f8cd89ad9960794847
zomvr <zomvroficial@gmail.com>
2022-03-10 18:53:56 -0300

    Update manageUpdates.js

--- public/js/manageUpdates.js
+++ public/js/manageUpdates.js
@@ -1,20 +1,47 @@
-document.querySelectorAll(".updated").forEach(updatedObj => {
-    const updatead = document.createElement("div");
-    updatead.innerText = "New Update!"
-    updatead.classList.add("update-ad");
-    updatedObj.appendChild(updatead);
-});
+if (location.href.includes("/es")) {
 
-document.querySelectorAll(".forgotten").forEach(updatedObj => {
-    const forgottenad = document.createElement("div");
-    forgottenad.innerText = "Forgotten."
-    forgottenad.classList.add("forgotten-ad");
-    updatedObj.appendChild(forgottenad);
-});
+    document.querySelectorAll(".updated").forEach(updatedObj => {
+        const updatead = document.createElement("div");
+        updatead.innerText = "Nueva Actualización!"
+        updatead.classList.add("update-ad");
+        updatedObj.appendChild(updatead);
+    });
+    
+    document.querySelectorAll(".forgotten").forEach(updatedObj => {
+        const forgottenad = document.createElement("div");
+        forgottenad.innerText = "Olvidado."
+        forgottenad.classList.add("forgotten-ad");
+        updatedObj.appendChild(forgottenad);
+    });
+    
+    document.querySelectorAll(".finished").forEach(updatedObj => {
+        const finishedad = document.createElement("div");
+        finishedad.innerText = "Finalizado."
+        finishedad.classList.add("finished-ad");
+        updatedObj.appendChild(finishedad);
+    });
 
-document.querySelectorAll(".finished").forEach(updatedObj => {
-    const finishedad = document.createElement("div");
-    finishedad.innerText = "Finished."
-    finishedad.classList.add("finished-ad");
-    updatedObj.appendChild(finishedad);
-});
+} else {
+
+    document.querySelectorAll(".updated").forEach(updatedObj => {
+        const updatead = document.createElement("div");
+        updatead.innerText = "New Update!"
+        updatead.classList.add("update-ad");
+        updatedObj.appendChild(updatead);
+    });
+    
+    document.querySelectorAll(".forgotten").forEach(updatedObj => {
+        const forgottenad = document.createElement("div");
+        forgottenad.innerText = "Forgotten."
+        forgottenad.classList.add("forgotten-ad");
+        updatedObj.appendChild(forgottenad);
+    });
+    
+    document.querySelectorAll(".finished").forEach(updatedObj => {
+        const finishedad = document.createElement("div");
+        finishedad.innerText = "Finished."
+        finishedad.classList.add("finished-ad");
+        updatedObj.appendChild(finishedad);
+    });
+
+}
