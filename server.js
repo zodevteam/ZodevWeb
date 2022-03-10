9595de5cfc69713dc4b3b3440c5a003d234f4c7b
zomvr <zomvroficial@gmail.com>
2022-03-09 21:13:08 -0300

    Zodev Web has been created.
    First Commit.

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const port = 3300 || process.env.PORT;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/html/eng/index.html")
+});
+
+app.get("/es", (req, res) => {
+    res.sendFile(__dirname + "/public/html/es/index.html")
+});
+
+app.listen(port);
