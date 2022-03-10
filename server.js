7175928486ffb427924bb743a527375b8d1b6d00
zomvr <zomvroficial@gmail.com>
2022-03-10 18:44:19 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const port = 3300 || process.env.PORT;
+const port = process.env.PORT;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
